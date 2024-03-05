6ea841f9ad6edfa727d1bfb23be9e11859c9c4c2
dksadasjkl <ckdgus6038@naver.com>
2024-03-05 17:46:18 +0900

    feat: react-quill

--- emotion_study/src/index.js
+++ emotion_study/src/index.js
@@ -9,13 +9,13 @@ import ComponentStudy from './compenents/pages/ComponentStudy/ComponentStudy';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <ComponentStudy a={10} b={20} c={50}/>
-  // <BrowserRouter>
-  //   <App />
-  // </BrowserRouter>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  // <ComponentStudy a={10} b={20} c={50}/>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+  );
+  
+  // If you want to start measuring performance in your app, pass a function
+  // to log results (for example: reportWebVitals(console.log))
+  // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+  reportWebVitals();
